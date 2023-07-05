a98e619d0fbe8acddb263a9e181def8f3fa97ad1
suniljat123 <sunilchoudharyjat45@gmail.com>
2023-07-05 15:38:24 +0530

    5.7

--- assets/js/MyComponents.js
+++ assets/js/MyComponents.js
@@ -42,8 +42,8 @@ class Footer extends HTMLElement {
           <div>
             <h4>Opening Hours</h4>
             <p>
-              <strong>Mon-Fri: </strong>10AM - 7PM<br>
-              Sun-Sat: Closed
+              <strong>Mon - Fri: </strong>10 AM - 7 PM IST<br>
+               <strong>Sun - Sat:</strong> Closed
             </p>
           </div>
         </div>
